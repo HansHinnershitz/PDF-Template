e61679cb2d7e8193c0d306b6d12deb7de26c187e
Hans <hjhinnershitz@gmail.com>
2024-07-23 22:09:15 -0400

    Added lines.

--- main.py
+++ main.py
@@ -15,12 +15,15 @@ for index, row in df.iterrows():
         pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
         pdf.line(x1=10, y1=21, x2=200, y2=21)
 
+        # Sets lines on header page
+        for y in range(31, 288, 10):
+            pdf.line(x1=10, y1=y, x2=200, y2=y)
+
         # Sets the footer
         pdf.ln(260)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1, border=0)
-
 
         for i in range(row["Pages"] - 1):
             pdf.add_page()
@@ -30,5 +33,9 @@ for index, row in df.iterrows():
             pdf.set_font(family="Times", style="I", size=8)
             pdf.set_text_color(180, 180, 180)
             pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1, border=0)
+
+            for y in range(31, 288, 10):
+                pdf.line(x1=10, y1=y, x2=200, y2=y)
+
 
 pdf.output("output.pdf")
