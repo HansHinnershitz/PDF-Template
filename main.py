648955638e71c681e1f1df8ad53a6f224cc07d6f
Hans <hjhinnershitz@gmail.com>
2024-07-23 21:32:33 -0400

    Added footers.

--- main.py
+++ main.py
@@ -4,16 +4,31 @@ from fpdf import FPDF
 df = pd.read_csv("topics.csv")
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 for index, row in df.iterrows():
         pdf.add_page()
 
+        # Sets the header
         pdf.set_font(family="Times", style="B", size=24)
         pdf.set_text_color(100, 100, 100)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
         pdf.line(x1=10, y1=21, x2=200, y2=21)
 
+        # Sets the footer
+        pdf.ln(260)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1, border=0)
+
+
         for i in range(row["Pages"] - 1):
             pdf.add_page()
+
+            # Sets the footer
+            pdf.ln(272)
+            pdf.set_font(family="Times", style="I", size=8)
+            pdf.set_text_color(180, 180, 180)
+            pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1, border=0)
 
 pdf.output("output.pdf")
